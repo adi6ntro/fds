7b119b1e484fa39af605d98ede9f652ba3bed168
Adi Guntoro <aoi.soichiro@gmail.com>
2020-07-06 10:25:49 +0700

    update model log

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ from datetime import datetime
 class Log(db.Model):
     __tablename__ = "t_log"
     id = db.Column(db.Integer, primary_key=True)
-    id_trans = db.Column(db.Integer)
+    id_trans = db.Column(db.String(250))
     date = db.Column(db.Date)
     time = db.Column(db.Time)
     company_id = db.Column(db.String(250))
@@ -31,12 +31,13 @@ class Log(db.Model):
     fraud_cause = db.Column(db.Text)
 
     def create(self):
-      db.session.add(self)
-      db.session.commit()
-      return self
-    def __init__(self,id_trans,date,time,company_id,source,destination,amount,
-        trans_type,product_id,ip_address,ip_city,ip_country,longitude,latitude,
-        geo_city,geo_state,geo_country,fraud_type,fraud_point,fraud_cause):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    def __init__(self, id_trans, date, time, company_id, source, destination, amount,
+                 trans_type, product_id, ip_address, ip_city, ip_country, longitude, latitude,
+                 geo_city, geo_state, geo_country, fraud_type, fraud_point, fraud_cause):
         self.id_trans = id_trans
         self.date = date
         self.time = time
@@ -61,6 +62,7 @@ class Log(db.Model):
     def __repr__(self):
         return '' % self.id_trans
 
+
 class User(UserMixin, db.Model):
     __tablename__ = "t_user"
     id = db.Column(db.Integer, primary_key=True)
@@ -72,28 +74,33 @@ class User(UserMixin, db.Model):
     is_active = db.Column(db.Integer)
 
     def create(self):
-      db.session.add(self)
-      db.session.commit()
-      return self
+        db.session.add(self)
+        db.session.commit()
+        return self
     # def __init__(self,username,email,password_hash):
     #     self.username = username
     #     self.email = email
     #     self.password_hash = password_hash
+
     def __repr__(self):
-        return '<User {}>'.format(self.username)    
+        return '<User {}>'.format(self.username)
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
+
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
 db.create_all()
+
 
 class LogSchema(ModelSchema):
     class Meta(ModelSchema.Meta):
         model = Log
         sqla_session = db.session
     id = fields.Integer(dump_only=True)
-    id_trans = fields.Integer(required=True)
+    id_trans = fields.String(required=True)
     date = fields.Date(required=True)
     time = fields.Time(required=True)
     company_id = fields.String(required=True)
@@ -114,6 +121,7 @@ class LogSchema(ModelSchema):
     fraud_point = fields.Integer(required=True)
     fraud_cause = fields.String(required=True)
     body_email = fields.String(required=False)
+
 
 @login.user_loader
 def load_user(id):
